55390739cb9de81a66e67feca72305553ece8ad3
Gabreelz <gabriel_teixeira30@hotmail.com>
2024-07-17 18:02:34 -0300

    Add files via upload

---
+++ Xadrez_System/src/application/program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Scanner;
+
+import boardgame.Position;
+
+public class program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Position posi = new Position(3, 5);
+		
+		System.out.println(posi);
+	}
+
+}
